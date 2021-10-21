c39d1af0cc8e9638c6af1aa96ba5fe1557e2ecbf
Paulius88 <82173700+Paulius88@users.noreply.github.com>
2021-10-21 12:01:25 +0300

    Add files via upload

---
+++ GitRepo/2021.10.20/ById.js
@@ -0,0 +1,14 @@
+let age_table = document.getElementById("age-table");
+    console.log(age_table);
+
+let age = document.getElementById("age");
+    console.log(age);
+
+let label_elements = document.getElementsByTagName("label");
+    console.log(label_elements);
+
+let name = document.getElementById("first");
+    console.log(name);
+
+let input_age = document.getElementById("age1");
+    console.log(input_age);
