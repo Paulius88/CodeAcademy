48d10bdef2e2b7db2413122300265fae21e428f1
Paulius88 <82173700+Paulius88@users.noreply.github.com>
2021-10-27 18:37:21 +0300

    Update 26.js

--- GitRepo/2021.10.26/26.js
+++ GitRepo/2021.10.26/26.js
@@ -65,37 +65,29 @@ let sliderImages = [
     'https://www.wallpaperbetter.com/wallpaper/379/200/45/climbing-hd-1080P-wallpaper.jpg'
 ]
 
-let image1 = document.createElement('img'),
-    image2 = document.createElement('img'),
-    image3 = document.createElement('img');
-
-    image2.style.display = 'none';
-    image3.style.display = 'none';
-
-    image1.src = sliderImages[0];
-    image2.src = sliderImages[1];
-    image3.src = sliderImages[2];
-
-    section_1.appendChild(image1);
-    section_1.appendChild(image2);
-    section_1.appendChild(image3);
-
-setInterval(() => {
-        if (image1.style.display === 'block') {
-            image1.style.display = 'none';
-            image2.style.display = 'block';
-            image3.style.display = 'none';
-        }
-        else if (image2.style.display === 'block') {
-            image1.style.display = 'none';
-            image2.style.display = 'none';
-            image3.style.display = 'block';
-        } else {
-        image1.style.display = 'block';
-        image2.style.display = 'none';
-        image3.style.display = 'none';
+let sliderImages = [
+    'https://divaofthemoment.com/wp-content/uploads/2016/01/slider-home-v5-background.jpg',
+    'https://i.pinimg.com/originals/0f/34/c5/0f34c5d0796f2c96ec945bfc01365006.jpg',
+    'https://i.pinimg.com/564x/9d/c1/63/9dc16311b623164894673456d865abc7.jpg',
+]
+for (let key in sliderImages) {
+   let image = document.createElement('img');
+    image.style.display = 'none';
+    image.src = sliderImages[key];
+    image.id = 'img-' + key;
+    section_1.appendChild(image);
+}
+let imgElements = section_1.querySelectorAll('img');
+let imgIndex = 0;
+function slide() {
+    for (let img of imgElements) {
+        img.style.display = 'none';
     }
-}, 2000)
+    let currentIndex = imgIndex++ % imgElements.length;
+    imgElements[currentIndex].style.display = 'block';
+    console.log(imgElements[currentIndex]);
+}
+setInterval(slide, 2000);
 
 function clock () {
     let fullDate = new Date();
