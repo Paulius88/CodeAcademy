db1ba0eb5437fad1ea1e82cfd85fa393e9a7bb5a
Paulius88 <82173700+Paulius88@users.noreply.github.com>
2021-11-04 18:47:03 +0200

    Add files via upload

---
+++ GitRepo/2010.11.04/props.js
@@ -0,0 +1,17 @@
+let auto = new Car(
+    'Skoda',
+    'Octavia',
+    2001
+);
+
+console.log(auto.getIntroduction());
+console.log(auto.getAge());
+
+let auto2 = new Car(
+    'Toyota',
+    'Rav4',
+    2019
+);
+
+console.log(auto2.getIntroduction());
+console.log(auto2.getAge());
