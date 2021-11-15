61cf27a36fa43d4250368c8f2016841d6a064ad9
Paulius88 <82173700+Paulius88@users.noreply.github.com>
2021-11-15 10:52:07 +0200

    Add files via upload

---
+++ WeatherApp/routes/weather.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let router = express.Router();
+
+let https = require('https');
+
+router.get('/places', function(req, res, next) {
+	https.get('https://api.meteo.lt/v1/places', response => {
+				let result = '';
+
+		response.on('data', function(data){		
+			result += data.toString();
+		});
+		response.on('end', function(data){		
+			res.send(result);
+		});
+	});
+});
+
+module.exports = router;
